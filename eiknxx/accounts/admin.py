d468d09e7bcb881001527673d601f7a9e114cb01
Alechnavičius Rokas <rokas.alechnavicius@mif.stud.vu.lt>
2018-04-25 12:56:40 +0300

    'de'

--- eiknxx/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, Collaborator
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Collaborator)
--- eiknxx/projektai/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task,Projektas, Old_Task,Old_Projektas, SyncedStuff
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(Projektas)
-admin.site.register(Old_Task)
-admin.site.register(Old_Projektas)
-admin.site.register(SyncedStuff)
